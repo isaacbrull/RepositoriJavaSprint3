42ce398705c731dcdb0e9f61122fb199415ce569
isaacbrull <61147625+isaacbrull@users.noreply.github.com>
2021-01-12 16:35:20 +0100

    Afegit perque si no me dona error
    sorry bros

--- Menu.java
+++ Menu.java
@@ -1,3 +1,5 @@
+package javasprint3.RepositoriJavaSprint3;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
